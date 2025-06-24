0dfe61fbf4df87f717779b73a978a39fcd44615b
Bibhuni123 <120247600+Bibhuni123@users.noreply.github.com>
2025-06-25 02:56:34 +0530

    Update main.tf
    asd

--- main.tf
+++ main.tf
@@ -1,24 +1,19 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+#without using count meta argument by using for each we are doing the same.
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+resource "aws_instance" "bibhu_instance" {
+  for_each = tomap({                    #here we define the condition with 2 type of ami
+    "bibhu-ami-id-micro" = "t2-micro"
+    "bibhu-ami-id-medium" = "t2-medium"
+  })
+  ami           = var.bibhu_ec2_ami_id
+  instance_type = each.value              # here we call them one by one
+
+  root_block_device {
+    volume_size = var.dev == "dev" ? 10 : var.default_root_volume_size
+    volume_type = var.bibhu_volume_type
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
 
   tags = {
-    Name = "HelloWorld"
+    Name = each.key                 # both of type will use one by one
   }
 }
