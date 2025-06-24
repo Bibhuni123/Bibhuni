6739274671efaaa7777cbea19ffb666172d8bc5f
Bibhuni123 <120247600+Bibhuni123@users.noreply.github.com>
2025-06-25 02:57:18 +0530

    Update and rename file1 to variable.tf
    asd

---
+++ variable.tf
@@ -0,0 +1,30 @@
+
+variable "default_root_volume_size" {
+  description = "The size of the root volume in GiB"
+  type        = number
+  default     = 20
+}
+variable "bibhu_volume_type" {
+  description = "Bibhu volume type is gp3"
+  type = string
+  default = "gp3"
+}
+variable "bibhu_instance_type" {
+  description = "Bibhu instance type is t2.micro"
+  type = string
+  default = "t2.micro"
+}
+variable "bibhu_ec2_ami_id" {
+  default = "ami-0b09627181c8d5778"
+  type = string
+}
+
+variable "prod" {
+  default = "prod"
+  type = string
+}
+
+variable "dev" {
+  default = "dev"
+  type = string
+}
